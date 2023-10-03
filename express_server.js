fcdfadf97dc494e8966141fd75826684096425e9
Yiqun Huang <albert913388672@gmail.com>
2023-10-02 23:09:00 -0700

    add the url_show file

--- express_server.js
+++ express_server.js
@@ -18,6 +18,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { id, longURL }
+  res.render("urls_show", templateVars)
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
