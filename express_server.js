cf2ba73108dd0ff189f4ae0397180122de1f93cf
Yiqun Huang <albert913388672@gmail.com>
2023-10-03 14:19:21 -0700

    add the delete feature

--- express_server.js
+++ express_server.js
@@ -59,7 +59,11 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
-
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect(`/urls`);
+})
 
 
 
